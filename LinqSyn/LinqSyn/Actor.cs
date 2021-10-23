eeb029f77b8c691e07f1ceec7466246b8a6b35d5
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-10-23 09:04:34 -0400

    linq lists

---
+++ LinqSyn/LinqSyn/Actor.cs
@@ -0,0 +1,35 @@
+namespace LinqSyn
+{
+    public class Actor
+    {
+        public string Name {get; set;}
+
+        public decimal TotalGross { get; set; }
+
+        public int MovieCount { get; set; }
+
+        public decimal AveragePerMovie { get; set; }
+
+        public string TopMovie {get; set;}
+
+        public decimal TopMovieGross {get; set;}
+
+
+        public override string ToString()
+        {
+            return "Name: " + Name +
+                (TotalGross != 0 ? " Total Gross: " + TotalGross : null) +
+                (MovieCount != 0 ? " Movie Count: " + MovieCount : null) +
+                (AveragePerMovie != 0 ? " Average Per Movie: " + AveragePerMovie : null) +
+                (TopMovie != null ? " Top Movie: " + TopMovie : null) +
+                (TopMovieGross != 0 ? " Top Movie Gross: " + TopMovieGross : null) + "\n";
+               
+        }
+
+
+
+    }
+
+
+
+}
